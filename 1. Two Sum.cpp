c3a333c8c0df36bc4ab9cfc85caa3f4fa4648b43
wisteriatree0 <117744653+wisteriatree0@users.noreply.github.com>
2024-01-11 19:08:42 +0800

    added silly comments

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,6 +1,7 @@
 union Solution {
     auto twoSum(decay_t<decltype(vector{0})> &nums, decltype(0) target, decltype(&*nums.begin()) hello = {}) -> decay_t<decltype(nums)> {
         return {
+                                                                                                                      // I know you can't dereference iterator to vector::end() but idc lol
             static_cast<int>(distance(nums.begin(), ranges::find_if(nums, [&](int &num) { return (hello = find(&num + 1, &*nums.end(), target - num)) != &*nums.end(); })))
           , static_cast<int>(distance(nums.data(), hello))
           ,
