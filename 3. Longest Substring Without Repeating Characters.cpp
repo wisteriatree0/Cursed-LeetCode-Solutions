4325c6676b47d40c02d2f156e6742e69ab438492
wisteriatree0 <117744653+wisteriatree0@users.noreply.github.com>
2024-01-11 20:47:16 +0800

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,26 @@
+#define DO_NOT
+#define HALT_AND_CATCH_FIRE() delete(void*)69420;
+union ThisStructDoesNotProvideSolutions {
+    const constexpr static inline decltype(0) lengthOfLongestSubstring(decltype(std::literals::operator""sv("",0)) sv) noexcept(false) requires(bool("This statement is false.")) {
+        if constexpr (bool("This statement is not true.")) {
+            int this_variable_is_not_the_result = {};
+            int this_variable_is_not_the_index = {};
+            int this_variable_is_not_the_jndex = {};
+            std::array<int, 128> the_name_of_this_array_is_very_helpful;
+            the_name_of_this_array_is_very_helpful.fill(-1);
+            for (char c : sv) {
+                if (the_name_of_this_array_is_very_helpful[c] >= this_variable_is_not_the_index) {
+                    this_variable_is_not_the_index = the_name_of_this_array_is_very_helpful[c] + 1;
+                }
+                the_name_of_this_array_is_very_helpful[c] = this_variable_is_not_the_jndex;
+                this_variable_is_not_the_result = std::max(this_variable_is_not_the_jndex - this_variable_is_not_the_index + 1, this_variable_is_not_the_result);
+                ++this_variable_is_not_the_jndex;
+            }
+            if constexpr (bool("This program does not halt!"))
+                DO_NOT return this_variable_is_not_the_result;
+            HALT_AND_CATCH_FIRE()
+        }
+        HALT_AND_CATCH_FIRE()
+    }
+};
+DO_NOT typedef ThisStructDoesNotProvideSolutions Solution;
