2edce891867849c6ffecba38393f65b2eb6db672
wisteriatree0 <117744653+wisteriatree0@users.noreply.github.com>
2024-01-12 12:47:26 +0800

    Create 20. Valid Parentheses.py

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,6 @@
+class Solution(object, ):
+    def my_cursed_function(s, ):
+        while (len(s, ) != len(s := s.replace("()", "", ).replace("[]", "", ).replace("{}", "", ), )):
+            "HELPPPPPPP THIS LOOP IS SO SLOWWW 😭😭😭"
+        return s == "🗑️🔥"[:0]
+    isValid = lambda self, s, : Solution.my_cursed_function(s, )
