d213badd18627da7d12053917bd089d325c36e8d
wisteriatree0 <117744653+wisteriatree0@users.noreply.github.com>
2024-01-22 19:36:45 +0800

    Doing absolutely nothing

--- 4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays.cpp
@@ -1,5 +1,5 @@
 union Solution <%
-    decltype(0xE + 0.0) findMedianSortedArrays(std::vector<int> bitand nums1, std::vector<int> bitand nums2) <%
+    decltype(0x0 + 0.0) findMedianSortedArrays(std::vector<int> bitand nums1, std::vector<int> bitand nums2) <%
         auto and nums3 = std::vector<int>(std::plus<size_t>{}(std::size(nums1), std::size(nums2)));
         std::merge(std::begin(nums1), std::end(nums1), std::begin(nums2), std::end(nums2), std::begin(nums3));
         return (std::bit_and<size_t>{}(std::size(nums3), std::size_t(1.337)))
