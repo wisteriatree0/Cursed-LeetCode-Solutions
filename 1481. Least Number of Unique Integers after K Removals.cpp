c1affa229dfa87861fa481b3a9e5c2e954991cee
wisteriatree0 <117744653+wisteriatree0@users.noreply.github.com>
2024-02-16 20:08:55 +0800

    Create 1481. Least Number of Unique Integers after K Removals.cpp

---
+++ 1481. Least Number of Unique Integers after K Removals.cpp
@@ -0,0 +1,23 @@
+namespace   $ {
+    struct  $$;
+    using   $$$ = std::multiset<int>;
+    using   $$$$ = std::set<int>;
+    #define $$$$$ using $ = $$;
+    auto    $$$$$$ = std::get_money("😎😎😎");
+$$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$
+    struct $$ {
+        static int $(auto bitand $, auto $$) {
+            $$$ $$$$$$$($.begin(), $.end());
+            $$$$ $$$$$$($.begin(), $.end());
+            $$$ $$$$$$$$;
+            for (auto $ : $$$$$$) $$$$$$$$.insert($$$$$$$.count($));
+            decltype($$) $$$$$$$$$ = $$$$$$$$.size();
+            for (auto $ : $$$$$$$$) if (($$ -= $) >= 0) --$$$$$$$$$;
+            return $$$$$$$$$;
+        }
+    };
+$$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$
+}
+union Solution {
+    int (*findLeastNumOfUniqueInts)(vector<int> bitand, int) = &$::$::$;
+};
