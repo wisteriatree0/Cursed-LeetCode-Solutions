0207d978511e2083f5e661dd86304badbb670e04
wisteriatree0 <117744653+wisteriatree0@users.noreply.github.com>
2024-04-09 22:01:04 +0800

    Create 60. Permutation Sequence.cpp

---
+++ 60. Permutation Sequence.cpp
@@ -0,0 +1,2 @@
+#include<ranges>🗣️🗣️🗣️🗣️🗣️🔥🔥🔥🥶🥶
+struct[[]]Solution{auto(getPermutation)(auto(n),auto(k)){return[&](auto(s)){return(iota(&s[0],&s[n],49),ranges::for_each(views::iota(-~*"",k),[&](auto){ranges::next_permutation(s);}),s);}(string(n,n));}};
